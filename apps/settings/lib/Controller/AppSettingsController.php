cf80cca54b5b6ba2b3dfe3066f72a338f8b97438
Julius Knorr <jus@bitgrid.net>
2026-01-14 22:35:13 +0100

    feat: Add office switcher with feature comparison
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- apps/settings/lib/Controller/AppSettingsController.php
+++ apps/settings/lib/Controller/AppSettingsController.php
@@ -93,6 +93,7 @@ class AppSettingsController extends Controller {
 		$this->initialState->provideInitialState('appstoreEnabled', $this->config->getSystemValueBool('appstoreenabled', true));
 		$this->initialState->provideInitialState('appstoreBundles', $this->getBundles());
 		$this->initialState->provideInitialState('appstoreUpdateCount', count($this->getAppsWithUpdates()));
+		$this->initialState->provideInitialState('isAllInOne', filter_var(getenv('THIS_IS_AIO'), FILTER_VALIDATE_BOOL));
 
 		if ($this->appManager->isEnabledForAnyone('app_api')) {
 			try {
